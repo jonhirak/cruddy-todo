96d8e899f99e00a7fb78a59d93217ed0e61f28c3
Takahiro Hirakawa <jonhirak@yahoo.com>
2021-10-12 10:13:00 -0700

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -82,14 +82,15 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+
+  var file = exports.dataDir + `/${id}.txt`;
+  fs.unlink(file, (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, id);
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
